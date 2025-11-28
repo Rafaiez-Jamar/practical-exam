edefdfd6e6a89ccf39da2e31097d9bcc073c8200
Rafaiez-Jamar <127466631+Rafaiez-Jamar@users.noreply.github.com>
2025-11-28 13:53:57 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Data siswa
+  String nama = "Mochammad Rafaiez";
+  String kelas = "XI RPL ";
+
+  // Menampilkan ke terminal
+  print("Nama Siswa  : $nama");
+  print("Kelas Siswa : $kelas");
+}
